f14c3e781d7ef3059a0bbea8bbbdd663711d83c4
Michael Simmelbauer <mas@Michaels-MBP.fritz.box>
2016-11-07 17:36:50 +0100

    changed back to var

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express = require("express");
+var express = require("express");
 
-const app = express();
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get("/", function(req, res) {
 
-  let ip = req.connection.remoteAddress;
-  let useragent = req.headers['user-agent'];
-  let lang = req.headers['accept-language'];
+  var ip = req.connection.remoteAddress;
+  var useragent = req.headers['user-agent'];
+  var lang = req.headers['accept-language'];
 
   //get the operating system out of useragent
-  let software = "";
+  var software = "";
   for(i=0; i < useragent.length; i++) {
-      let placeholder = useragent.indexOf(")");
+      var placeholder = useragent.indexOf(")");
       if (useragent[i] === "(" ) {
           for(j = i+1; j < placeholder; j++) {
               software += useragent[j];
@@ -21,16 +21,16 @@ app.get("/", function(req, res) {
       }
   }
   //get the language
-  let language = lang.split(",")[0];
+  var language = lang.split(",")[0];
 
-  let api = {
+  var api = {
     "ipaddress": ip,
     "lanugage": language,
     "software": software
   }
   res.send(api);
 })
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'))
